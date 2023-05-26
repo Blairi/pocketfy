ec3e8f83982856017c04d5099d1d453b7a929d6e
Blairi <axel.avi.fer@gmail.com>
2023-05-25 22:40:10 -0600

    Backend boilerplate

---
+++ backend/src/main/java/com/pocketfy/pocketfy/PocketfyApplication.java
@@ -0,0 +1,13 @@
+package com.pocketfy.pocketfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketfyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pocketfy/pocketfy/PocketfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocketfy.pocketfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
