e13f7fbca4530d560c9f9e4bdcd65ac6491be4e7
Blairi <axel.avi.fer@gmail.com>
2023-05-27 22:39:56 -0600

    retrieve accounts by user id

---
+++ backend/src/main/java/com/pocketfy/pocketfy/account/AccountController.java
@@ -0,0 +1,22 @@
+package com.pocketfy.pocketfy.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("users/{id}/accounts")
+    public List<AccountDTO> retrieveAccountsByUserId(@PathVariable Long id) {
+        return accountService.getAccountsByUserId(id);
+    }
+}
