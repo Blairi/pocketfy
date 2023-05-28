947eee0addf72ac9dbab6178c5d70fb931d15278
Blairi <axel.avi.fer@gmail.com>
2023-05-27 22:39:36 -0600

    implementing basic account service method

---
+++ backend/src/main/java/com/pocketfy/pocketfy/account/AccountServiceImpl.java
@@ -0,0 +1,34 @@
+package com.pocketfy.pocketfy.account;
+
+import com.pocketfy.pocketfy.user.UserDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+@Service
+public class AccountServiceImpl implements AccountService{
+
+    private static List<AccountDTO> accounts = new ArrayList<>();
+    private static Long accountCount = 0L;
+
+    static {
+        accounts.add(
+          new AccountDTO(++accountCount, "visa", 1L)
+        );
+        accounts.add(
+                new AccountDTO(++accountCount, "cash", 1L)
+        );
+    }
+
+    @Override
+    public List<AccountDTO> getAccountsByUserId(Long id) {
+        Predicate<? super AccountDTO> predicate =
+                account -> account.getUserId().equals(id);
+
+        return accounts.stream().filter(predicate).collect(Collectors.toList());
+    }
+}
