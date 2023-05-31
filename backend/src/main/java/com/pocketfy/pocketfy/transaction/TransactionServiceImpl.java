d91b86cbe617877ea30774058993481e47f8d89a
Blairi <axel.avi.fer@gmail.com>
2023-05-30 21:57:00 -0600

    Implementing a basic account service

---
+++ backend/src/main/java/com/pocketfy/pocketfy/transaction/TransactionServiceImpl.java
@@ -0,0 +1,37 @@
+package com.pocketfy.pocketfy.transaction;
+
+import com.pocketfy.pocketfy.account.AccountDTO;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Service
+public class TransactionServiceImpl implements TransactionService{
+
+    private static Long transactionCount = 0L;
+    private static List<TransactionDTO> transactions = new ArrayList<>();
+
+    static {
+        transactions.add(
+                new TransactionDTO(++transactionCount, "Clothes and transport", LocalDate.now(), BigDecimal.valueOf(-2000), 1L, 1L, 1L)
+        );
+    }
+
+    @Override
+    public TransactionDTO getTransactionById(Long id) {
+
+        Predicate<? super TransactionDTO> predicate =
+                transaction -> transaction.getUserId().equals(id);
+
+        Optional<TransactionDTO> transaction = transactions.stream().filter(predicate).findFirst();
+
+        if(transaction.isEmpty()) throw new RuntimeException("Transaction not found.");
+
+        return transaction.get();
+    }
+}
