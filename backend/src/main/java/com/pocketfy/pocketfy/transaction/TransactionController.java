32377195c614913771528c626d5d26d29b279099
Blairi <axel.avi.fer@gmail.com>
2023-05-30 21:57:35 -0600

    Retrieve account by id

---
+++ backend/src/main/java/com/pocketfy/pocketfy/transaction/TransactionController.java
@@ -0,0 +1,35 @@
+package com.pocketfy.pocketfy.transaction;
+
+import com.pocketfy.pocketfy.account.AccountDTO;
+import com.pocketfy.pocketfy.account.AccountService;
+import com.pocketfy.pocketfy.user.UserDTO;
+import com.pocketfy.pocketfy.user.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransactionController {
+
+    private TransactionService transactionService;
+    private UserService userService;
+    private AccountService accountService;
+
+    public TransactionController(TransactionService transactionService, UserService userService, AccountService accountService) {
+        this.transactionService = transactionService;
+        this.userService = userService;
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/users/{userId}/accounts/{accountId}/transactions/{transactionId}")
+    public TransactionDTO retrieveTransactionById(
+            @PathVariable Long userId, @PathVariable Long accountId, @PathVariable Long transactionId) {
+
+        // TODO: Check if transaction belong to the user
+        UserDTO user = userService.getUserById(userId);
+        AccountDTO account = accountService.getAccountById(accountId);
+
+        return transactionService.getTransactionById(transactionId);
+    }
+
+}
