e90d1521c3f378270579cc3cadfb363d93613233
Blairi <axel.avi.fer@gmail.com>
2023-06-04 22:39:52 -0600

    Get all transactions by user id method signature

--- backend/src/main/java/com/pocketfy/pocketfy/transaction/TransactionService.java
+++ backend/src/main/java/com/pocketfy/pocketfy/transaction/TransactionService.java
@@ -1,7 +1,11 @@
 package com.pocketfy.pocketfy.transaction;
 
+import java.util.List;
+
 public interface TransactionService {
 
     public TransactionDTO getTransactionById(Long id);
+
+    public List<TransactionDTO> getTransactionsByUserId(Long userId);
 
 }
