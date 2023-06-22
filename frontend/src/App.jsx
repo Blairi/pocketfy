dc338fb6242bb14ed431bbb0fbfcac9d144d18bf
Blairi <axel.avi.fer@gmail.com>
2023-06-22 13:54:39 -0600

    Included animatecss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { AppRouter } from './router/AppRouter';
-import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import 'animate.css';
 
 export default function App() {
   return (
