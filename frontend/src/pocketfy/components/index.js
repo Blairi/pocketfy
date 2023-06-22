23764844cc369a69bf965e96340eacbfbc8e07bd
Blairi <axel.avi.fer@gmail.com>
2023-06-22 15:30:20 -0600

    components

--- frontend/src/pocketfy/components/index.js
+++ frontend/src/pocketfy/components/index.js
@@ -2,3 +2,6 @@ export * from "./Navbar";
 export * from "./Brand";
 export * from "./DateSelection";
 export * from "./PieChart";
+export * from "./TransactionForm";
+export * from "./NewTransaction";
+export * from "./Home";
