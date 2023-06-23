205de28eb80d54ec072071094ea91829c2520957
Blairi <axel.avi.fer@gmail.com>
2023-06-23 17:56:17 -0600

    Conditionally showing the drawer

--- frontend/src/pocketfy/pages/PocketfyPage.jsx
+++ frontend/src/pocketfy/pages/PocketfyPage.jsx
@@ -1,14 +1,21 @@
-import { Home } from "../components"
+import { useContext } from "react";
+import { FilterDrawer, Home, Navbar } from "../components"
+import { FilterContext } from "../contexts/FilterContext";
 import { TransactionFormProvider } from "../contexts/TransactionFormProvider"
 import { PocketfyLayout } from "../layout/PocketfyLayout"
 
 export const PocketfyPage = () => {
+
+  const { showFilter } = useContext(FilterContext);
 
   return (
     <PocketfyLayout>
 
       <TransactionFormProvider>
 
+        {showFilter && <FilterDrawer />}
+
+        <Navbar />
         <Home /> 
 
       </TransactionFormProvider>
