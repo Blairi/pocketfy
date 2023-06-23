5268137e5696be1a289cfd104d87b418b27c0791
Blairi <axel.avi.fer@gmail.com>
2023-06-23 17:54:24 -0600

    Filter context

---
+++ frontend/src/pocketfy/contexts/FilterProvider.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { FilterContext } from "./FilterContext";
+
+export const FilterProvider = ({ children }) => {
+
+  const [showFilter, setShowFilter] = useState(false);
+
+  return (
+    <FilterContext.Provider value={{showFilter, setShowFilter}}>
+      { children }
+    </FilterContext.Provider>
+  )
+}
