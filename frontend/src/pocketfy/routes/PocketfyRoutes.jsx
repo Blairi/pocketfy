4fc0316c1bb576b66b83476be30ef4763d8cc2c5
Blairi <axel.avi.fer@gmail.com>
2023-06-23 17:55:26 -0600

    Using filter context

--- frontend/src/pocketfy/routes/PocketfyRoutes.jsx
+++ frontend/src/pocketfy/routes/PocketfyRoutes.jsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import { PocketfyPage } from "../pages/PocketfyPage"
+import { FilterProvider } from "../contexts/FilterProvider";
+
 
 export const PocketfyRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<PocketfyPage />}/>
+      
+        <Route path="/" element={
+          <FilterProvider><PocketfyPage /></FilterProvider>
+        }/>
     </Routes>
   )
 }
