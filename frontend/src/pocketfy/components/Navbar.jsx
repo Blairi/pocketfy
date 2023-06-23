007fdd23f0b8ab5ed137b689174d74a7718a4c27
Blairi <axel.avi.fer@gmail.com>
2023-06-23 17:54:08 -0600

    Changing to filter icon

--- frontend/src/pocketfy/components/Navbar.jsx
+++ frontend/src/pocketfy/components/Navbar.jsx
@@ -1,19 +1,25 @@
+import { useContext } from "react"
 import { Brand } from "./"
+import { FilterContext } from "../contexts/FilterContext"
 
 export const Navbar = () => {
+
+  const { setShowFilter } = useContext(FilterContext);
+
   return (
     <div className="navbar bg-base-100">
 
       <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-          </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Homepage</a></li>
-            <li><a>Portfolio</a></li>
-            <li><a>About</a></li>
-          </ul>
+        <div className="flex-none">
+          <button
+            className="btn btn-square btn-ghost"
+            onClick={() => setShowFilter(true)}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+            </svg>
+
+          </button>
         </div>
       </div>
 
