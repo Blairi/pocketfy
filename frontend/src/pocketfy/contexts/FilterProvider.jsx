af4313483e3a1fd44663d6ce30735984d449d463
Blairi <axel.avi.fer@gmail.com>
2023-06-24 18:22:26 -0600

    filter and date states

--- frontend/src/pocketfy/contexts/FilterProvider.jsx
+++ frontend/src/pocketfy/contexts/FilterProvider.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react"
 import { FilterContext } from "./FilterContext";
+import dayjs from 'dayjs'
 
 export const FilterProvider = ({ children }) => {
 
   const [showFilter, setShowFilter] = useState(false);
 
+  const [filterSelected, setFilterSelected] = useState("day");
+  
+  const [date, setDate] = useState(dayjs());
+
   return (
-    <FilterContext.Provider value={{showFilter, setShowFilter}}>
+    <FilterContext.Provider value={
+      {showFilter, setShowFilter, date, setDate, filterSelected, setFilterSelected}
+    }>
       { children }
     </FilterContext.Provider>
   )
