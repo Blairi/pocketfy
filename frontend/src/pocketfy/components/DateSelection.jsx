04d115864cb0e2234bc0379ae2f594c50f755412
Blairi <axel.avi.fer@gmail.com>
2023-06-24 18:25:02 -0600

    Setting the amount according to the filter

--- frontend/src/pocketfy/components/DateSelection.jsx
+++ frontend/src/pocketfy/components/DateSelection.jsx
@@ -1,15 +1,43 @@
-import { useState } from "react"
-import dayjs from 'dayjs'
+import { useContext, useState } from "react"
+import { FilterContext } from "../contexts/FilterContext";
+import { useEffect } from "react";
 
 export const DateSelection = () => {
 
-  const [date, setDate] = useState(dayjs().format('DD/MMM/YYYY'));
+  const { date, setDate, filterSelected } = useContext(FilterContext);
+
+  const [manipulate, setManipulate] = useState({amount: null, unit: null});
+
+  useEffect(() => {
+
+    if(filterSelected === "day") {
+      setManipulate({amount: 1, unit: "day"});
+    }
+
+    if(filterSelected === "week") {
+      setManipulate({amount: 7, unit: "day"});
+    }
+
+    if(filterSelected === "month") {
+      setManipulate({amount: 30, unit: "day"});
+    }
+
+  }, [filterSelected]);
 
   return (
     <nav className="flex justify-between items-center">
-      <button className="btn btn-primary">&#10094;</button>
-      <span className="font-black">{date}</span>
-      <button className="btn btn-primary">&#10095;</button>
+
+      <button 
+        className="btn btn-primary" 
+        onClick={() => setDate(date.subtract(manipulate.amount, manipulate.unit))}
+      >&#10094;</button>
+
+      <span className="font-black">{date.format("DD/MMM/YYYY")}</span>
+
+      <button 
+        className="btn btn-primary" 
+        onClick={() => setDate(date.add(manipulate.amount, manipulate.unit))}
+      >&#10095;</button>
     </nav>
   )
 }
