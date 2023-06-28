c3e820bf8950d000886d183a34f4e953e079c6b0
Blairi <axel.avi.fer@gmail.com>
2023-06-28 10:48:28 -0600

    Adding hard coded accounts

--- frontend/src/service/local/loadLocalAccounts.js
+++ frontend/src/service/local/loadLocalAccounts.js
@@ -3,7 +3,13 @@ export const loadLocalAccounts = () => {
   let accounts = localStorage.getItem("accounts");
 
   if(!accounts) {
-    localStorage.setItem("accounts", JSON.stringify( [{id: 1, name: "default"}] ));
+    localStorage.setItem("accounts", JSON.stringify([
+      // TODO: Remove hard coded accounts
+      {id: -1, name: "all accounts"},
+      {id: 1, name: "default"},
+      {id: 2, name: "visa"},
+      {id: 3, name: "cash"},
+    ]));
     accounts = localStorage.getItem("accounts");
   }
 
