1bee21f71fcb437b1a1ad7c5e54064ed2bd12404
Blairi <axel.avi.fer@gmail.com>
2023-06-28 10:44:42 -0600

    Select account reducer implemented

--- frontend/src/store/pocketfy/PocketfySlice.js
+++ frontend/src/store/pocketfy/PocketfySlice.js
@@ -3,14 +3,18 @@ import { createSlice } from '@reduxjs/toolkit';
 export const pocketfySlice = createSlice({
   name: 'pocketfy',
   initialState: {
-    accounts: []
+    accounts: [],
+    accountSelected: null,
   },
   reducers: {
     setAccounts: (state, action ) => {
       state.accounts = action.payload;
     },
+    setSelectedAccount: (state, action) => {
+      state.accountSelected = action.payload;
+    },
   }
 });
 
 // Action creators are generated for each case reducer function
-export const { setAccounts } = pocketfySlice.actions;
+export const { setAccounts, setSelectedAccount } = pocketfySlice.actions;
