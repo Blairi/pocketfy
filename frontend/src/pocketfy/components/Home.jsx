5f872ffe2d33af9013f76fab1c4f0463fedd224c
Blairi <axel.avi.fer@gmail.com>
2023-06-28 10:46:10 -0600

    Display the selected account

--- frontend/src/pocketfy/components/Home.jsx
+++ frontend/src/pocketfy/components/Home.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react"
 import { DateSelection, NewTransaction, PieChart, TransactionForm } from "../components"
 import { TransactionFormContext } from "../contexts/TransactionFormContext";
+import { useSelector } from "react-redux";
 
 export const Home = () => {
 
   const { showTransactionForm } = useContext(TransactionFormContext);
+  const { accountSelected } = useSelector(state => state.pocketfy);
 
   return (
     <>
@@ -17,8 +19,16 @@ export const Home = () => {
 
           : (
             <div className="animate__animated animate__fadeInUp space-y-10">
+
               <DateSelection />
+
+              <div>
+                <span className="text-xs">Account Selected</span>
+                <h2 className="text-lg text-primary font-black">{accountSelected?.name}</h2>
+              </div>
+
               <PieChart />
+
               <NewTransaction />
             </div>
           )
