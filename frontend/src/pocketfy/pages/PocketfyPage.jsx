05e0c829c14828705dd164748fab84b8a300eb87
Blairi <axel.avi.fer@gmail.com>
2023-06-28 10:51:03 -0600

    Setting the accounts and selected account

--- frontend/src/pocketfy/pages/PocketfyPage.jsx
+++ frontend/src/pocketfy/pages/PocketfyPage.jsx
@@ -1,12 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { FilterDrawer, Home, Navbar } from "../components"
 import { FilterContext } from "../contexts/FilterContext";
 import { TransactionFormProvider } from "../contexts/TransactionFormProvider"
 import { PocketfyLayout } from "../layout/PocketfyLayout"
+import { useDispatch } from 'react-redux';
+import { startLoadingAccounts, startSelectAccount } from "../../store/pocketfy/thunks";
 
 export const PocketfyPage = () => {
 
   const { showFilter } = useContext(FilterContext);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch( startLoadingAccounts() );
+    dispatch( startSelectAccount(-1) );
+  }, [])
 
   return (
     <PocketfyLayout>
