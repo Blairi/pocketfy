a70096607017424f87cbe8554f51539af0f8c088
Blairi <axel.avi.fer@gmail.com>
2023-07-10 22:14:31 -0600

    Cleaning up unnecessary state properties

--- frontend/src/pocketfy/contexts/FilterProvider.jsx
+++ frontend/src/pocketfy/contexts/FilterProvider.jsx
@@ -1,18 +1,13 @@
 import { useState } from "react"
 import { FilterContext } from "./FilterContext";
-import dayjs from 'dayjs'
 
 export const FilterProvider = ({ children }) => {
 
   const [showFilter, setShowFilter] = useState(false);
 
-  const [filterSelected, setFilterSelected] = useState("day");
-  
-  const [date, setDate] = useState(dayjs());
-
   return (
     <FilterContext.Provider value={
-      {showFilter, setShowFilter, date, setDate, filterSelected, setFilterSelected}
+      {showFilter, setShowFilter}
     }>
       { children }
     </FilterContext.Provider>
