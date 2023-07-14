e5c0cc408612bcd87dae63ff63bc60f69bb68c52
Blairi <axel.avi.fer@gmail.com>
2023-07-13 21:57:08 -0600

    Transaction list component

--- frontend/src/pocketfy/components/TransactionList.jsx
+++ frontend/src/pocketfy/components/TransactionList.jsx
@@ -2,19 +2,31 @@
 export const TransactionList = ({ transactions = [] }) => {
   console.log(transactions);
   return (
-    <div>
-      {
-        transactions.map((transaction, index) => (
-          <div className="flex gap-5">
-          <div>
-            {transaction.category}
-          </div>
-          <div>
-            {transaction.amount}
-          </div>
-          </div>
-        ))
-      }
+    <div className="">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Category</th>
+            <th>Account</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            transactions.map((transaction, index) => (
+              <tr
+                key={ index }
+              >
+                <th>{transaction.date}</th>
+                <th>{transaction.category}</th>
+                <th>{transaction.account}</th>
+                <th>{transaction.amount}</th>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
