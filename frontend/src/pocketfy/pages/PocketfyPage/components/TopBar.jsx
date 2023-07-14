b8a2978160d45bd7a9190467ce6dea51b709e9ec
Blairi <axel.avi.fer@gmail.com>
2023-07-14 10:59:37 -0600

    Top bar component

--- frontend/src/pocketfy/pages/PocketfyPage/components/TopBar.jsx
+++ frontend/src/pocketfy/pages/PocketfyPage/components/TopBar.jsx
@@ -1,25 +1,22 @@
-import { useContext } from "react"
-import { Brand } from "./"
-import { FilterContext } from "../contexts/FilterContext"
+import { Brand } from "../../../../components"
 
-export const Navbar = () => {
-
-  const { setShowFilter } = useContext(FilterContext);
+export const TopBar = () => {
 
   return (
     <div className="navbar bg-base-100">
 
       <div className="navbar-start">
         <div className="flex-none">
-          <button
-            className="btn btn-square btn-ghost"
-            onClick={() => setShowFilter(true)}
+
+          <label
+            htmlFor="my-drawer"
+            className="btn btn-square btn-ghost drawer-button"
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
             </svg>
+          </label> {/* Open filter drawer button */}
 
-          </button>
         </div>
       </div>
 
