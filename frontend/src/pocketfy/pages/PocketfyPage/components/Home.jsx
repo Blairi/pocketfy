ee3a4159c2c05de33e674795e936c03dc13d440d
Blairi <axel.avi.fer@gmail.com>
2023-07-14 16:06:07 -0600

    Home component

---
+++ frontend/src/pocketfy/pages/PocketfyPage/components/Home.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux"
+import { DateSelection, NewTransaction, PieChart, TopBar } from "./"
+
+export const Home = () => {
+
+  const { accountSelected } = useSelector(state => state.pocketfy);
+
+  return (
+    <main className="space-y-10 mt-5">
+
+      <div className="animate__animated animate__fadeInUp space-y-10">
+
+        <TopBar />
+
+        <DateSelection />
+
+        <div>
+          <span className="text-xs">Account Selected</span>
+          <h2 className="text-lg text-primary font-black">{accountSelected?.name}</h2>
+        </div>
+
+        <PieChart />
+
+        <NewTransaction />
+
+      </div>
+
+    </main>
+  )
+}
