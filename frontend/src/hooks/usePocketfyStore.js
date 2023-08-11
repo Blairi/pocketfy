5035f852b18f90da4606c47aaedc8a8c5d1e2b07
Blairi <axel.avi.fer@gmail.com>
2023-08-11 09:56:12 -0600

    finishing the custom hook

--- frontend/src/hooks/usePocketfyStore.js
+++ frontend/src/hooks/usePocketfyStore.js
@@ -1,14 +1,47 @@
 import { useDispatch, useSelector } from "react-redux"
+import { startLoadingApp, startSelectAccount, startSetActiveTransactionsByDateFilter, startSetTransaction } from "../store/pocketfy/thunks";
 
 export const usePocketfyStore = () => {
   const dispatch = useDispatch();
 
   const {
+    accounts,
+    accountSelected,
+    categories,
+    dateFilterSelected, 
+    activeDate,
+    activeTransactions,
   } = useSelector(state => state.pocketfy);
+
+  const onStartLoadingApp = () => {
+    dispatch( startLoadingApp() );
+  }
+
+  const onStartSetTransaction = (transaction) => {
+    dispatch( startSetTransaction(transaction) );
+  }
+
+  const onStartSelectAccount = (id) => {
+    dispatch( startSelectAccount(id) );
+  }
+
+  const onStartSetActiveTransactionsByDateFilter = (date, filter) => {
+    dispatch( startSetActiveTransactionsByDateFilter(date, filter) );
+  }
 
   return {
     // Properties
+    accounts,
+    accountSelected,
+    categories,
+    dateFilterSelected, 
+    activeDate,
+    activeTransactions,
 
     //Methods
+    onStartLoadingApp,
+    onStartSetTransaction,
+    onStartSelectAccount,
+    onStartSetActiveTransactionsByDateFilter,
   }
 }
