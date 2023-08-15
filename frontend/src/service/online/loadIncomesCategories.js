fb31ddecb942623d2b344ccb5ebffaac63df0ec3
Blairi <axel.avi.fer@gmail.com>
2023-08-15 09:22:49 -0600

    load two types of categories - web services

---
+++ frontend/src/service/online/loadIncomesCategories.js
@@ -0,0 +1,21 @@
+
+export const loadIncomesCategories = async () => {
+  return [
+    {
+      id: 1,
+      name: "salary",
+    },
+    {
+      id: 2,
+      name: "gifts",
+    },
+    {
+      id: 3,
+      name: "allowance",
+    },
+    {
+      id: 4,
+      name: "Bonus",
+    },
+  ];
+}
