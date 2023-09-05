5f1578070e888eaf940cc7b56c6f9692fb2e1126
Blairi <axel.avi.fer@gmail.com>
2023-09-05 10:59:12 -0600

    transactions resume component

--- frontend/src/pocketfy/pages/PocketfyPage/components/Resume.jsx
+++ frontend/src/pocketfy/pages/PocketfyPage/components/Resume.jsx
@@ -1,40 +1,25 @@
-
-export const Resume = () => {
+export const Resume = ({ type, amounts }) => {
   return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        {/* head */}
-        <thead>
-          <tr>
-            <th>Category</th>
-            <th>Amount</th>
-            <th>Percentage</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* row 1 */}
-          <tr>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
-          </tr>
-          {/* row 2 */}
-          <tr>
-            <th>2</th>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
-          </tr>
-          {/* row 3 */}
-          <tr>
-            <th>3</th>
-            <td>Brice Swyre</td>
-            <td>Tax Accountant</td>
-            <td>Red</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="bg-neutral rounded-md space-y-2 animate__animated animate__backInUp">
+      {
+        Object.keys(amounts[type]).map((category, index) => (
+          <div
+            key={index}
+            className="flex justify-between text-neutral-content px-2 py-3"
+          >
+            <h6 className="capitalize">
+              {category}
+            </h6>
+            <span className="text-white">
+              {
+                amounts[type][category] < 0
+                  ? `-$${amounts[type][category] * -1}`
+                  : `$ ${amounts[type][category]}`
+              }
+            </span>
+          </div>
+        ))
+      }
     </div>
   )
 }
