94d2012e0e53b4f107c9108cfdba0303470eac1c
Blairi <axel.avi.fer@gmail.com>
2023-09-06 19:52:13 -0600

    using loading overlay

--- frontend/src/pocketfy/pages/PocketfyPage/PocketfyPage.jsx
+++ frontend/src/pocketfy/pages/PocketfyPage/PocketfyPage.jsx
@@ -1,10 +1,17 @@
+import { LoadingOverlay } from "../../../components/ui";
+import { usePocketfyStore } from "../../../hooks";
 import { PocketfyLayout } from "../../layout/PocketfyLayout";
 import { Home } from "./components";
 
 export const PocketfyPage = () => {
+  const { isLoading } = usePocketfyStore();
   return (
     <PocketfyLayout>
-      <Home />
+      {
+        !isLoading 
+          ? <Home />
+          : <LoadingOverlay />
+      }
     </PocketfyLayout>
   )
 }
