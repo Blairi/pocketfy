27af23beb78f7d8f1b9d0e7da08d70b826335565
Blairi <axel.avi.fer@gmail.com>
2023-09-18 20:21:40 -0600

    use params in the component

--- frontend/src/pocketfy/pages/TransactionPage/ResumeTransactionsPage.jsx
+++ frontend/src/pocketfy/pages/TransactionPage/ResumeTransactionsPage.jsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom";
 
 export const ResumeTransactionsPage = () => {
+
+  const { categoryId } = useParams();
+
   return (
     <div>ResumeTransactionsPage</div>
   )
