66970790e971846fb8758b4c4c6f93a1b23a91db
Blairi <axel.avi.fer@gmail.com>
2023-10-05 22:47:44 -0600

    removing unnecesary account

--- frontend/src/service/local/loadLocalAccounts.js
+++ frontend/src/service/local/loadLocalAccounts.js
@@ -4,8 +4,6 @@ export const loadLocalAccounts = () => {
 
   if(!accounts) {
     localStorage.setItem("accounts", JSON.stringify([
-      // TODO: Remove hard coded accounts
-      {id: -1, name: "all accounts"},
       {id: 1, name: "default"},
       {id: 2, name: "visa"},
       {id: 3, name: "cash"},
