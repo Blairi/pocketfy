2a2c144509fb744b388efc40dfcfe1615e4168d0
Blairi <axel.avi.fer@gmail.com>
2024-04-22 16:37:21 -0600

    adding link to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { AppRouter } from './router/AppRouter';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import 'animate.css';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <h1>Testing deploy</h1>
+      <Link to="/home" className="underline">Home</Link>
       <AppRouter />
     </BrowserRouter>
   )
